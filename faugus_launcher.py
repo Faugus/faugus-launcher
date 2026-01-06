1079be0a41ce817b54ce667af4b3ab62b287d5ba
Faugus <felix.ribeiro@live.com>
2026-01-06 12:49:00 -0300

    Version 1.12.2

--- faugus_launcher.py
+++ faugus_launcher.py
@@ -26,7 +26,7 @@ from filelock import FileLock, Timeout
 from faugus.config_manager import *
 from faugus.dark_theme import *
 
-VERSION = "1.12.1"
+VERSION = "1.12.2"
 print(f"Faugus Launcher {VERSION}")
 IS_FLATPAK = 'FLATPAK_ID' in os.environ or os.path.exists('/.flatpak-info')
 
