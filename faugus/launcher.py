4a0da8d7b32b9844855b672166c61b9eb3489ea0
Faugus <felix.ribeiro@live.com>
2026-04-15 12:14:29 -0300

    Version 1.18.1

--- faugus/launcher.py
+++ faugus/launcher.py
@@ -22,7 +22,7 @@ from faugus.dark_theme import *
 from faugus.steam_setup import *
 from faugus.ea_fix import *
 
-VERSION = "1.17.5"
+VERSION = "1.18.1"
 IS_FLATPAK = 'FLATPAK_ID' in os.environ or os.path.exists('/.flatpak-info')
 
 faugus_banner = PathManager.system_data('faugus-launcher/faugus-banner.png')
