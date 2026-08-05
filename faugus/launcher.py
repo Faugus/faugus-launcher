dae1342b0882e1f9594e5ba760d999ab432e66e8
Faugus <felix.ribeiro@live.com>
2026-08-05 11:36:28 -0300

    Version 2.0.6

--- faugus/launcher.py
+++ faugus/launcher.py
@@ -22,7 +22,7 @@ from faugus.ea_fix import *
 from faugus.tray_sni import TrayIcon
 from faugus.migration import fix_legacy_shortcut_icons
 
-VERSION = "2.0.5"
+VERSION = "2.0.6"
 
 if IS_FLATPAK:
     tray_icon = 'io.github.Faugus.faugus-launcher'
